f5766730066bc2548c0c6a3beaae948aa934d889
ramedus <81530995+ramedus@users.noreply.github.com>
2022-07-23 18:11:14 +0300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+#Set the working directory and place source file in the path
+hs_data<-read.table("household_power_consumption.txt",sep=";",na.strings="?",header=T)
+hs_data$datetime<-strptime(paste(hs_data[,1],hs_data[,2]),"%d/%m/%Y %H:%M:%S")
+library(dplyr)
+household_data<-filter(hs_data,datetime>="2007-02-01" & datetime<"2007-02-03")
+png("plot3.png")
+with(household_data,plot(datetime,Sub_metering_1,type="n",ylab="Energy sub metering",xlab=""))
+with(household_data,lines(datetime,Sub_metering_1))
+with(household_data,lines(datetime,Sub_metering_2,col="red"))
+with(household_data,lines(datetime,Sub_metering_3,col="blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch=45,col=c("black","red","blue"))
+dev.off()
